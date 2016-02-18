412cfe8cebd9518361cd589dc0e9b16d6c135665
andersonmcook <andersonmcook@gmail.com>
2016-02-18 09:54:08 -0600

    created new express server

---
+++ server.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+'use strict';
+
+// const bodyParser = require('body-parser');
+const express = require('express');
+// const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server Running');
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Evernode running on port: ${PORT}`);
+});
