bd268d52a4b8501e3a300cf2480da0d8e0976bb1
andersonmcook <andersonmcook@gmail.com>
2016-02-18 12:35:15 -0600

    fixed issue with app.use

--- routes/note.js
+++ routes/note.js
@@ -2,26 +2,16 @@
 
 const express = require('express');
 const router = express.Router();
-const Note = require('../models/note');
+const note = require('../controllers/note');
 
-router.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-// post new note to db and redirect to home
-router.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-    res.redirect(`/notes/${note._id}`);
-  });
-});
+router.get('/notes/new', note.newNote);
 
 // routes with route params need to be below static routes
-router.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) res.send('404');
-    res.render('show-note', {note: note});
-  });
-});
+router.get('/notes/:id', note.show);
+// router.delete('/notes/:id', note.destroy);
+
+// post new note to db and redirect to home
+router.post('/notes', note.create);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,22 +8,23 @@ const note = require('./routes/note');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//use /note routes
-app.use(note);
-
-// set view engine to jade
-app.set('view engine', 'jade');
-
 // bodyParser middleware
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
 app.use(bodyParser.json());
+
+// set view engine to jade
+app.set('view engine', 'jade');
 
 //home route
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
+
+//use /note routes. needs to be below middleware
+app.use(note);
 
 //connect database then listen to server
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
