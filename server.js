581ff28f26c0ea4c287c0166333c8926d7fc23e8
andersonmcook <andersonmcook@gmail.com>
2016-02-18 10:52:49 -0600

    save note to db

--- server.js
+++ server.js
@@ -7,12 +7,22 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
+
+
+// set view engine to jade
 app.set('view engine', 'jade');
 
+// bodyParser middleware
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(bodyParser.json());
 
+//home route
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
@@ -22,10 +32,17 @@ app.get('/notes/new', (req, res) => {
 });
 
 app.post('/notes', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
+    res.redirect('/');
+  });
 });
 
-app.listen(PORT, ()=>{
-  console.log(`Evernode running on port: ${PORT}`);
+//connect database then listen to server
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+  app.listen(PORT, ()=>{
+    console.log(`Evernode running on port: ${PORT}`);
+  });
 });
