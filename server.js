ab8b0916249d1c0d3650326b760de700ca25fca8
andersonmcook <andersonmcook@gmail.com>
2016-02-18 10:34:57 -0600

    added form and routes for form

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 'use strict';
 
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/', (req, res) => {
   res.send('Server Running');
+});
+
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(PORT, ()=>{
